d985f2ac0ef92d7ca8a45f603af65a8b0d91ba87
milcho0604 <milcho0604@gmail.com>
2024-06-11 15:26:08 +0900

    set

--- src/C01Basic/C10Set.java
+++ src/C01Basic/C10Set.java
@@ -1,4 +1,79 @@
 package C01Basic;
 
+import java.util.*;
+
 public class C10Set {
+    public static void main(String[] args) {
+//        Set<String> mySet = new HashSet<>();
+//        mySet.add("h");
+//        mySet.add("h");
+//        mySet.add("e");
+//        mySet.add("l");
+//        mySet.add("l");
+//        mySet.add("o");
+//        System.out.println(mySet);
+
+        // nums에서 중복되지 않은 2개의 경우의수
+//        int[] nums = {3,3,3,2,2,4]};
+//        Set<Integer> mySet = new HashSet<>();
+//        int count =0;
+//        for (int i=0; i<nums.length; i++) {
+//            mySet.add(nums[i]);
+//        }
+//        List<Integer> list = new ArrayList<>(mySet);
+//        for(int i=0; i<list.size(); i++){
+//            for (int j=i+1; j<list.size(); j++){
+//                count++;
+//            }
+//        }
+//        if(count > nums.length/2){
+//            System.out.println(nums.length/2);
+//        }else {
+//            System.out.println(count);
+//        }
+//        int[] nums = {3,1,2,3};
+//        Set<Integer> mySet = new HashSet<>();
+//        int count =0;
+//        for (int i=0; i<nums.length; i++) {
+//            mySet.add(nums[i]);
+//        }
+//        int answer =0;
+//
+//        if(mySet.size()>nums.length/2){
+//            answer =nums.length/2;
+//        }
+//        else {
+//            answer = mySet.size();
+//        }
+
+//        List<String> myList = new ArrayList<>();
+//        myList.add("temnis");
+//        myList.add("base");
+//        myList.add("ball");
+//        myList.add("ball");
+//
+////        list를 인자값으로 받아서 초긱밧 세팅 가능
+//        Set<String> mySet = new HashSet<>(myList);
+//        System.out.println(mySet);
+//
+//        String[] arr = {"Base", "bb", "saq"};
+//        Set<String> mySet2 = new HashSet<>(Arrays.asList(arr));
+//        mySet2.remove("Base");
+//        System.out.println(mySet2);
+
+//        프로그래머스 : 전화번호 목록
+//        String[] phone_book = {"12", "123", "1235", "567", "88"};
+//        boolean answer = true;
+//        Arrays.sort(phone_book);
+//        System.out.println(Arrays.toString(phone_book));
+//        for (int i = 0; i < phone_book.length - 1; i++) {
+//            if (phone_book[i + 1].startsWith(phone_book[i])) {
+//                answer = false;
+//                break;
+//            }
+//        }
+//        System.out.println(answer);
+
+
+    }
 }

e3e18b37658f520059457ce5cdc13f35b1a4b9e8
milcho0604 <milcho0604@gmail.com>
2024-06-11 16:02:07 +0900

    Que..Deque..Stack..

--- src/C01Basic/C10Set.java
+++ src/C01Basic/C10Set.java
@@ -2,6 +2,8 @@ package C01Basic;
 
 import java.util.*;
 
+import static java.util.List.*;
+
 public class C10Set {
     public static void main(String[] args) {
 //        Set<String> mySet = new HashSet<>();
@@ -73,6 +75,48 @@ public class C10Set {
 //            }
 //        }
 //        System.out.println(answer);
+//        String[] phone_book = {"12", "123", "1235", "567", "88"};
+//        Set<String> set = new HashSet<>(Arrays.asList(phone_book));
+//        boolean answer = true;
+//
+//        for(String s : phone_book){
+//            for(int i=0; i<s.length(); i++){
+//                if(set.contains(s.substring(0,i)));
+//                answer = false;
+//                break;
+//            }
+//        }
+
+//        집합관련함수 : 교집함(retainAll), 합집합(addAll), 차집합(removeAll)
+//        Set<String> set1 = new HashSet<>(Arrays.asList("java","python", "C++"));
+//        Set<String> set2 = new HashSet<>(Arrays.asList("list","hash", "C++"));
+//
+////        set1.retainAll(set2);
+////        set1.addAll(set2);
+//        set1.removeAll(set2);
+//        System.out.println(set1);
+//        관련문제 : 뉴스 클러스터링
+
+//        Set<String> mySet = new TreeSet<>();
+//        mySet.add("hello5");
+//        mySet.add("hello5");
+//        mySet.add("hello3");
+//        mySet.add("hello1");
+//        mySet.add("hello1");
+//        System.out.println(mySet);
+
+//        두개 뽑아서 더하기
+        int[] numbers = {2,1,3,4,1};
+        Set<Integer> set = new TreeSet<>();
+        for(int i=0; i<numbers.length; i++){
+            for(int j=i+1; j<numbers.length; j++){
+                set.add(numbers[i]+numbers[j]);
+            }
+        }
+        System.out.println(set);
+
+
+
 
 
     }
---
+++ src/C01Basic/C11QueStackDeque.java
@@ -0,0 +1,4 @@
+package C01Basic;
+
+public class C11QueStackDeque {
+}

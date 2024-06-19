dbef09ea3079c8431f4923d252bd762553a190c6
milcho0604 <milcho0604@gmail.com>
2024-06-19 17:40:18 +0900

    finish Thread

---
+++ src/C08Thread/MainClass.java
@@ -0,0 +1,68 @@
+package C08Thread;
+
+public class MainClass {
+    public static void main(String[] args) throws InterruptedException {
+////        스레드 생성방법 2가지 :
+////        1. 스레드 상속방식
+//        Thread t1 = new Thread1();
+////        start메서드가 상속되어 있고, 이 메서드를 통해 rub메서드 실행
+//        t1.start();
+//        Thread t2 = new Thread1();
+//        t2.start();
+//
+//
+////        2. Runnable 구현한 객체를 Thread 클래스에 주입 방식
+//        Thread t3 = new Thread(new Thread2());
+//        t3.start();
+//        new Thread(new Thread2()).start();
+//        new Thread(() ->
+//                System.out.println("익명Runnable객체 주입 후 실해행 Thread"))
+//                .start();
+//        위와 아래가 같음
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("익명Runnable객체 주입 후 실해행 Thread");
+//            }
+//        }).start();
+
+
+//        Thread 동시성 이슈 테스트
+//        for(int i=0; i<1000; i++){
+//            Library.borrow();
+//        }
+//        System.out.println("최종적으로 남은 수량: "+ Library.bookCount);
+
+//        for(int i=0; i<5000 ; i++){
+//            new Thread(()-> {
+//                try {
+//                    Library.borrow();
+//                } catch (InterruptedException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            }).start();
+//        }
+//        System.out.println("최종적으로 남은 수량: "+ Library.bookCount);
+
+
+
+////        동시성 이슈 해결방법 첫 번째. join() 메서드 사용
+////        join(): 다른 스레드의 완료전까지 새로운 스레드가 실행되지 않음
+//        for(int i=0; i<1000; i++) {
+//            Thread t1 = new Thread(() -> Library.borrow());
+//            t1.start();
+//            //        동시성 이슈 해결방법 첫 번째. join() 메서드 사용
+//            //        join(): 다른 스레드의 완료전까지 새로운 스레드가 실행되지 않음
+//            t1.join();
+//            System.out.println("최종적으로 남은 수량: " + Library.bookCount);
+//        }
+
+
+//        두 번째 : synchronized
+        for(int i=0; i<1000; i++) {
+            Thread t1 = new Thread(() -> Library.borrow());
+            t1.start();
+            System.out.println("최종적으로 남은 수량: " + Library.bookCount);
+        }
+    }
+}
---
+++ src/C08Thread/Thread2.java
@@ -0,0 +1,9 @@
+package C08Thread;
+
+
+public class Thread2 extends Thread{
+    @Override
+    public void run() {
+        System.out.println("Thread2 실행 시작");
+    }
+}
